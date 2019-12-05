57d931545de4979710f88e5ce791890e2f66543f
Inna Nikolenko <innaivasyuk93@gmail.com>
2019-12-04 21:54:16 -0800

    Created tests for Modal component

---
+++ __tests__/components/Modal.test.jsx
@@ -0,0 +1,36 @@
+/* global describe, it, expect, jest */
+
+import { shallow } from 'enzyme';
+import React from 'react';
+import Modal from '../../client/src/components/Modal';
+
+describe('Modal tests', () => {
+  const photosMock = [
+    'https://immersive-photo-bucket.s3.us-west-2.amazonaws.com/images/10/one.jpeg',
+    'https://immersive-photo-bucket.s3.us-west-2.amazonaws.com/images/10/two.jpeg',
+    'https://immersive-photo-bucket.s3.us-west-2.amazonaws.com/images/10/three.jpeg',
+    'https://immersive-photo-bucket.s3.us-west-2.amazonaws.com/images/10/four.jpeg',
+    'https://immersive-photo-bucket.s3.us-west-2.amazonaws.com/images/10/five.jpeg',
+    'https://immersive-photo-bucket.s3.us-west-2.amazonaws.com/images/10/six.jpeg',
+  ];
+
+  const changeModalViewFn = jest.fn();
+  const nextImageFn = jest.fn();
+  const previousImageFn = jest.fn();
+
+  const wrapper = shallow(<Modal
+    photos={photosMock}
+    changeModalView={changeModalViewFn}
+    nextImage={nextImageFn}
+    previousImage={previousImageFn}
+  />);
+
+  it('should render modal', () => {
+    expect(wrapper.find('.modal-container')).toHaveLength(1);
+    expect(wrapper.find('.mask')).toHaveLength(1);
+  });
+
+  it('should render modal content', () => {
+    expect(wrapper.find('ModalContent')).toHaveLength(1);
+  });
+});
